70e1acd191a61223f7982c39147c3745154d0d8d
laxmankeshatwar <laxmankeshatwar@outlook.com>
2026-09-09 14:58:46 +0530

    Some changes

--- SalesApi/Program.cs
+++ SalesApi/Program.cs
@@ -37,8 +37,6 @@ sales.MapPost("/", (SaleCreate create, ISalesRepository repo, HttpContext http) 
 sales.MapPut("/{id:int}", (int id, SaleUpdate update, ISalesRepository repo) =>
 {
     var ok = repo.Update(id, update);
-    
-    
     return ok ? Results.NoContent() : Results.NotFound();
 });
 
